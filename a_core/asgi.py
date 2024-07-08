615ff6e9b49cde41c1b39d676a9b955726ba4ba0
godfrey-tankan <godfreytantswakandeya@gmail.com>
2024-07-08 16:27:29 +0200

    chore: Update ASGI configuration to use Django's default ASGI application and add support for HTTP requests

--- a_core/asgi.py
+++ a_core/asgi.py
@@ -10,7 +10,12 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'a_core.settings')
 
-application = get_asgi_application()
+django_asgi_app = get_asgi_application()
+application = get_asgi_application({
+    "http":django_asgi_app,
+})
+
