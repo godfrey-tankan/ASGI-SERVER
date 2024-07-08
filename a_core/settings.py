f92080cb2f1494c22067f6654a2bc080a8520b91
godfrey-tankan <godfreytantswakandeya@gmail.com>
2024-07-08 15:40:15 +0200

    chore: Add 'daphne' to INSTALLED_APPS in settings.py

--- a_core/settings.py
+++ a_core/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -83,7 +84,8 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'a_core.wsgi.application'
+# WSGI_APPLICATION = 'a_core.wsgi.application'
+ASGI_APPLICATION = 'a_core.asgi.application'
 
 
 # Database
