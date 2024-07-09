205b9a78046cc549f404f67ed0c8f57227f7d1a4
godfrey-tankan <godfreytantswakandeya@gmail.com>
2024-07-09 08:54:55 +0200

    chore: Update ASGI configuration to use Django's default ASGI application and add support for WebSocket communication

--- a_core/asgi.py
+++ a_core/asgi.py
@@ -11,11 +11,24 @@ import os
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from channels.auth import AuthMiddlewareStack
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'a_core.settings')
 
 django_asgi_app = get_asgi_application()
+
+from a_core import routing
+
 application = get_asgi_application({
     "http":django_asgi_app,
+    "websocket":AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                routing.websocket_urlpatterns
+            )
+        )
+    )
 })
+
 
